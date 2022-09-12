ccb7d3f6920335748fbbc92c75f5bd6d16a13d93
Oskar-Przyborski <oskar.przski@gmail.com>
2022-09-12 21:24:22 +0200

    updated deps

--- pages/_document.js
+++ pages/_document.js
@@ -2,13 +2,14 @@ import Document, { Head, Main, NextScript, Html } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage((App) => (props) =>
+    const page = ctx.renderPage((App) => (props) =>
       sheet.collectStyles(<App {...props} />),
     );
     const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...page, styleTags, ...initialProps };
   }
 
   render() {
